6b59aff192e0ef00241e4a66c49de6ceea4af06a
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-08-11 10:18:48 +0530

    refactor:name added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import "./App.css";
 
 function App() {
-  return <h1>Hello Hello world React</h1>;
+  return;
+  <h1>Hello Hello world React</h1>;
+  <h1>Welcome Siddharth</h1>;
 }
 
 export default App;
