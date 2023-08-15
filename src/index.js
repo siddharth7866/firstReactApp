4e550828dca276f225a24416570ce2878cab7ea1
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-08-15 13:18:31 +0530

    RestrictMode

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React! How are you siddharth!</h1>;
+  const sid = "siddharth";
+  return <h1>Hello React! How are you {sid}!</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <reactstrict>
+    <App />
+  </reactstrict>
+);
