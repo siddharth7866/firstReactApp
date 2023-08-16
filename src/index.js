fb6f26169ba3e5d604e41943766c1332d19666f6
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-08-16 11:30:58 +0530

    CreatingComponents

--- src/index.js
+++ src/index.js
@@ -3,7 +3,26 @@ import ReactDOM from "react-dom/client";
 
 function App() {
   const sid = "siddharth";
-  return <h1>Hello React! How are you {sid}!</h1>;
+  return (
+    <div>
+      <h1>Hello React! How are you {sid}!</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/salamino.jpg" alt="pizza salamino" />
+      <h1>"Pizza Spinaci"</h1>
+      <p>
+        <strong>"Tomato, mozarella, ham, aragula, and burrata cheese"</strong>
+      </p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
